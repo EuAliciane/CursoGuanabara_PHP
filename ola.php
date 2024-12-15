baba46e282115498c4c6394d00d2d590d21347a4
EuAliciane <98620470+EuAliciane@users.noreply.github.com>
2024-12-14 21:10:55 -0300

    primeiro commit PHP

---
+++ ola.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Meu primeiro Olá Mundo</title>
+    <style>
+        h1{
+            color:#80a2ff;
+           text-shadow: 5px 5px 5px;
+        }
+    </style>
+</head>
+<body>
+    <div>
+        <?php
+        echo "<h1>Olá, Mundo!</h2>";
+        ?>
+    </div>
+</body>
+</html>
